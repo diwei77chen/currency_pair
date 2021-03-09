3bd5ff5e483015349f4a0a09e61ed8033ba47c63
diwei77chen <diwei77chen@gmail.com>
2021-03-09 12:13:43 +1100

    prettier single quote: true

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+  },
 };
