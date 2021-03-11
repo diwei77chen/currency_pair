d16b57ebd31c47fa313909154a167fa806e67d78
diwei77chen <diwei77chen@gmail.com>
2021-03-11 15:42:59 +1100

    Create NewQuoteButton.test.js

---
+++ src/scenes/QuoteResult/NewQuoteButton.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { render, shallow } from 'enzyme';
+import Button from '@material-ui/core/Button';
+import NewQuoteButton from './NewQuoteButton';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Quote Result New Quote Button Test Suite', () => {
+  test('renders with the correct submit text', () => {
+    const wrapper = render(<NewQuoteButton />);
+    expect(wrapper.text()).toContain('START NEW QUOTE');
+  });
+
+  // test('simulates click', () => {
+  //   const clickMe = jest.fn();
+  //   const wrapper = shallow(<NewQuoteButton onNewQuotePress={clickMe} />);
+  //   wrapper.find(Button).simulate('click');
+  //   expect(clickMe).toBeCalled();
+  // });
+});
